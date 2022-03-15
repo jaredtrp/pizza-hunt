e18d5de32888f136a0f5cc6b1a86b64151724afb
Jared <jaredtrp@gmail.com>
2022-03-14 20:53:34 -0500

    added latest module code

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -15,11 +15,17 @@ const handleAddTopping = event => {
   checkbox.type = 'checkbox';
   checkbox.name = 'topping';
   checkbox.value = toppingValue;
-  checkbox.id = toppingValue.toLowerCase().split(' ').join('-');
+  checkbox.id = toppingValue
+    .toLowerCase()
+    .split(' ')
+    .join('-');
 
   const label = document.createElement('label');
   label.textContent = toppingValue;
-  label.htmlFor = toppingValue.toLowerCase().split(' ').join('-');
+  label.htmlFor = toppingValue
+    .toLowerCase()
+    .split(' ')
+    .join('-');
 
   const divWrapper = document.createElement('div');
 
@@ -36,9 +42,7 @@ const handlePizzaSubmit = event => {
   const pizzaName = $pizzaForm.querySelector('#pizza-name').value;
   const createdBy = $pizzaForm.querySelector('#created-by').value;
   const size = $pizzaForm.querySelector('#pizza-size').value;
-  const toppings = [
-    ...$pizzaForm.querySelectorAll('[name=topping]:checked'),
-  ].map(topping => {
+  const toppings = [...$pizzaForm.querySelectorAll('[name=topping]:checked')].map(topping => {
     return topping.value;
   });
 
@@ -47,21 +51,23 @@ const handlePizzaSubmit = event => {
   }
 
   const formData = { pizzaName, createdBy, size, toppings };
+
   fetch('/api/pizzas', {
     method: 'POST',
     headers: {
       Accept: 'application/json',
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(formData),
+    body: JSON.stringify(formData)
   })
     .then(response => response.json())
     .then(postResponse => {
-      alert('Pizza created successfully!');
       console.log(postResponse);
     })
     .catch(err => {
       console.log(err);
+      saveRecord(formData);
+      // DO INDEXED DB STUFF HERE
     });
 };
 
